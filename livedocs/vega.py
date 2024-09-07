792e503066e59f2d565a1f772f2125012555ec7d
matthewconnell <matthew.at.connell@gmail.com>
2024-09-07 09:40:50 -0700

    Working full stacked bar charts

--- livedocs/vega.py
+++ livedocs/vega.py
@@ -1247,6 +1247,79 @@ def swapped_main_chart(
                 # ]
             )
         )
+    elif mark_type == "full_stacked_bar":
+        brush = alt.selection_interval(encodings=["y"])
+        select = alt.selection_point(name="select", on="click")
+        highlight = alt.selection_point(
+        name="highlight", on="pointerover", empty=False
+        )
+
+        if color_by_field:
+            base_layer = (
+                alt.Chart(df)
+                .mark_bar(clip=True, filled=True, cursor="pointer")
+                .encode(
+                    x=x_encoding.stack("normalize"),
+                    y=y_encoding,
+                    color=alt.condition(brush, color_by_encoding, alt.value('lightgray')),
+                    opacity=opacity_encoding,
+                    fillOpacity=alt.condition(select, opacity_encoding, alt.value(0.3)),
+                    tooltip=[
+                        alt.Tooltip(
+                            field=y_field,
+                            type=y_type,
+                            title=y_field,
+                            timeUnit=y_temporal_format if y_temporal_format else alt.Undefined
+                        ),
+                        alt.Tooltip(
+                            field=x_field,
+                            type=x_type,
+                            title=x_field
+                            if x_aggregate == "none"
+                            else f'{x_aggregate} of  {x_field}',
+                            aggregate=x_aggregate
+                            if x_aggregate != "none"
+                            else alt.Undefined,
+                        ),
+                        alt.Tooltip(
+                            field=color_by_field,
+                            type=color_by_type,
+                            title=color_by_field,
+                            aggregate=color_by_aggregate
+                            if color_by_aggregate != "none"
+                            else alt.Undefined,
+                        )
+                    ],
+                )
+            ).add_params(select, highlight, brush)
+
+        else:
+            base_layer = (
+                alt.Chart(df)
+                .mark_bar(clip=True, filled=True, cursor="pointer")
+                .encode(
+                    x=x_encoding,
+                    y=y_encoding,
+                    color=color_by_encoding,
+                    opacity=opacity_encoding,
+                    tooltip=[
+                        alt.Tooltip(
+                            field=y_field,
+                            type=y_type,
+                            title=y_field,
+                            timeUnit=y_temporal_format if y_temporal_format else alt.Undefined
+                        ),
+                        alt.Tooltip(
+                            field=x_field,
+                            type=x_type,
+                            title=x_field,
+                            aggregate=x_aggregate
+                            if x_aggregate != "none"
+                            else alt.Undefined
+                        )
+                    ],
+                )
+            )
 
     chart = alt.layer(base_layer)
 
