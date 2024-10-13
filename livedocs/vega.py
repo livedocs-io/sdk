2aecef6ce31faef48bd72382d6d7ed5b515b3bee
matthewconnell <matthew.at.connell@gmail.com>
2024-10-13 01:56:41 -0700

    Fixed style settings

--- livedocs/vega.py
+++ livedocs/vega.py
@@ -632,7 +632,6 @@ def main_chart(
         if mark_type == "grouped_column":
 
             if color_by_aggregate:
-                print(color_by_sort)
                 base_layer = (
                     alt.Chart(df)
                     .mark_bar(clip=True,
@@ -720,7 +719,7 @@ def main_chart(
 
 
         elif mark_type == "stacked_column":
-
+            print(style_settings)
             if color_by_aggregate:
                 base_layer = (
                     alt.Chart(df)
@@ -1731,35 +1730,50 @@ def create_x_encoding(
     x_aggregate: str = "none",
     horizontal: bool = False,
 ):
-    tick_count_expr = "length(domain('x')) > 0 ? min(ceil(width / 40), ceil((domain('x')[1] - domain('x')[0]) / 7884000000)) : ceil(width / 40)"
     axis_settings = style.get("xAxis", {})
 
-    axis_props = alt.Axis(
-        title=field if "xAxis" not in style else style["xAxis"].get("title", field),
-        titleFontSize=style.get("fontSize", 10),
-        labelFontSize=style.get("fontSize", 10),
-        labelAngle=axis_settings.get("labelAngle", alt.Undefined),
-        tickCount=axis_settings.get(
-            "ticks", alt.expr(tick_count_expr) if not horizontal else alt.Undefined
-        ),
-        grid=True if axis_settings.get("grid", "none") != "none" else False,
-        gridDash=[4, 4]
-        if axis_settings.get("grid", "none") == "dashed"
-        else alt.Undefined,
-        labelOverlap=True
-    )
 
     if type == "temporal" and temporal_format and temporal_format != "none":
-        axis_props.format = get_axis_format(temporal_format)
+        return alt.X(
+            f"{x_aggregate}({field}):T" if x_aggregate != "none" else f"{field}:T",
+            timeUnit=temporal_format,
+            axis=alt.Axis(
+                title=field
+                if "xAxis" not in style
+                else style["xAxis"].get("title", field),
+                format=get_axis_format(temporal_format),
+                titleFontSize=style.get("fontSize", 10),
+                labelFontSize=style.get("fontSize", 10),
+                labelAngle=axis_settings.get("labelAngle", alt.Undefined),
+                tickCount=axis_settings.get("ticks", alt.Undefined),
+                grid=True if axis_settings.get("grid", "none") != "none" else True,
+                gridDash=[4, 4]
+                if axis_settings.get("grid", "none") == "dashed"
+                else alt.Undefined,
+                labelOverlap=True
+            ),
+        )
+    else:
         return alt.X(
             f"{x_aggregate}({field}):{type}"
             if x_aggregate != "none"
             else f"{field}:{type}",
-            timeUnit=temporal_format,
-            bandPosition=0,
-            aggregate=x_aggregate if x_aggregate != "none" else alt.Undefined,
-            sort=sort,
-            axis=axis_props,
+            axis=alt.Axis(
+                title=field
+                if "xAxis" not in style
+                else style["xAxis"].get("title", field),
+                titleFontSize=style.get("fontSize", 10),
+                labelFontSize=style.get("fontSize", 10),
+                labelAngle=axis_settings.get("labelAngle", alt.Undefined),
+                tickCount=axis_settings.get("ticks", alt.Undefined),
+                grid=True if axis_settings.get("grid", "none") != "none" else True,
+                format=axis_settings.get("format", alt.Undefined),
+                gridDash=[4, 4]
+                if axis_settings.get("grid", "none") == "dashed"
+                else alt.Undefined,
+                labelOverlap=True
+
+            ),
             scale=alt.Scale(
                 domainMax=int(axis_settings["max"])
                 if "max" in axis_settings
@@ -1770,15 +1784,61 @@ def create_x_encoding(
                 type=axis_settings.get("scale", alt.Undefined),
             ),
         )
-    else:
-        return alt.X(
-            f"{x_aggregate}({field}):{type}"
-            if x_aggregate != "none"
-            else f"{field}:{type}",
-            sort=sort,
-            aggregate=x_aggregate if x_aggregate != "none" else alt.Undefined,
-            axis=axis_props,
-        )
+
+    # tick_count_expr = "length(domain('x')) > 0 ? min(ceil(width / 40), ceil((domain('x')[1] - domain('x')[0]) / 7884000000)) : ceil(width / 40)"
+    # axis_settings = style.get("xAxis", {})
+
+    # axis_props = alt.Axis(
+    #     title=field if "xAxis" not in style else style["xAxis"].get("title", field),
+    #     titleFontSize=style.get("fontSize", 10),
+    #     labelFontSize=style.get("fontSize", 10),
+    #     ##
+    #     # format=style.get('xAxis', {}).get('format', "%b %Y"),
+    #     ##
+    #     labelAngle=axis_settings.get("labelAngle", alt.Undefined),
+    #     tickCount=axis_settings.get(
+    #         "ticks", alt.expr(tick_count_expr) if not horizontal else alt.Undefined
+    #     ),
+    #     grid=True if axis_settings.get("grid", "none") != "none" else False,
+    #     gridDash=[4, 4]
+    #     if axis_settings.get("grid", "none") == "dashed"
+    #     else alt.Undefined,
+    #     labelOverlap=True
+    # )
+
+    # if type == "temporal" and temporal_format and temporal_format != "none":
+    #     axis_props.format=get_axis_format(temporal_format),
+
+    #     return alt.X(
+    #         f"{x_aggregate}({field}):{type}" if x_aggregate != "none" else f"{field}:{type}",
+    #         timeUnit=temporal_format,
+    #         ##
+    #         # format=format,
+    #         ##
+    #         bandPosition=0,
+    #         aggregate=x_aggregate if x_aggregate != "none" else alt.Undefined,
+    #         sort=sort,
+    #         axis=axis_props,
+    #         scale=alt.Scale(
+    #             domainMax=int(axis_settings["max"])
+    #             if "max" in axis_settings
+    #             else alt.Undefined,
+    #             domainMin=int(axis_settings["min"])
+    #             if "min" in axis_settings
+    #             else alt.Undefined,
+    #             type=axis_settings.get("scale", alt.Undefined),
+    #         ),
+    #     )
+    # else:
+    #     # axis_props.format = style.get('xAxis', {}).get('format', alt.Undefined),
+    #     return alt.X(
+    #         f"{x_aggregate}({field}):{type}"
+    #         if x_aggregate != "none"
+    #         else f"{field}:{type}",
+    #         sort=sort,
+    #         aggregate=x_aggregate if x_aggregate != "none" else alt.Undefined,
+    #         axis=axis_props,
+    #     )
 
 
 def create_y_encoding(
@@ -1820,6 +1880,7 @@ def create_y_encoding(
                 labelAngle=axis_settings.get("labelAngle", alt.Undefined),
                 tickCount=axis_settings.get("ticks", alt.Undefined),
                 grid=True if axis_settings.get("grid", "none") != "none" else True,
+                format=axis_settings.get("format", alt.Undefined),
                 gridDash=[4, 4]
                 if axis_settings.get("grid", "none") == "dashed"
                 else alt.Undefined,
